eed15e43d32b65c2e7f51feed053533f508f6f4e
kmjh386 <129623676+kmjh386@users.noreply.github.com>
2023-04-01 13:53:54 -0600

    Add files via upload

---
+++ Card Array.js
@@ -0,0 +1,80 @@
+/* 
+
+Plasma
+1 Pillar
+2 Chariot
+3 Armor
+4 Crown
+5 Bow
+6 Chalice
+7 Scepter
+8 Star
+9 Sun
+10 Moon
+
+Cosmos
+1 Pillar
+2 Chariot
+3 Armor
+4 Crown
+5 Bow
+6 Chalice
+7 Scepter
+8 Star
+9 Sun
+10 Moon 
+
+Entropy
+1 Pillar
+2 Chariot
+3 Armor
+4 Crown
+5 Bow
+6 Chalice
+7 Scepter
+8 Star
+9 Sun
+10 Moon
+
+Æther
+1 Pillar
+2 Chariot
+3 Armor
+4 Crown
+5 Bow
+6 Chalice
+7 Scepter
+8 Star
+9 Sun
+10 Moon
+
+Opal
+20
+
+Onxy
+20
+
+Jasper
+30
+
+Sapphire
+30
+
+Emerald
+30
+
+Quartz
+30
+
+Knave 1
+
+Acolyte 2
+Mystic 3
+Seer 4
+Maiden 5
+Knight 6
+King 8
+Fool 9
+Queen 7
+*/
+
